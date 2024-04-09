cb12d61572f33d2c1828c2444cd374be2e2f76fc
Madhu <madhu8129@gmail.com>
2024-04-09 23:28:14 +0200

    first React app

---
+++ src/HelloWorld.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function HelloWorld() {
+        return (
+            <div>
+                <h1>Hello World!!!<br></br>My First React App</h1>
+            </div>
+        );
+}
+export default HelloWorld;
